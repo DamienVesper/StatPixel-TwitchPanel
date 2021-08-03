39038ae1fbf38a6a640dd43eff7b2392d3ee5c83
Damien Vesper <ldamienvesper@gmail.com>
2021-08-02 21:23:51 -0500

    add a page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../../public/assets/scss/pages/dashboard.scss';
+
+/**
+ * The settings dashbaord.
+ */
+class Dashboard extends React.Component {
+    render = () => {
+        return (
+            <main className="text-center container mt-5">
+            </main>
+        );
+    }
+}
+
+export default Dashboard;
